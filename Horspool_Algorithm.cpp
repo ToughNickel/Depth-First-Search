dedcecba59cfc4ff875be4525a3bc2d16165d092
Nikhil Raj <rajnikhil870@gmail.com>
2016-06-01 18:57:17 +0530

    made to work on TurboC++

--- Horspool_Algorithm.cpp
+++ Horspool_Algorithm.cpp
@@ -58,11 +58,15 @@ void Case :: randomize()
    Timer t;
    for(int i = 1000;i <= 10000;i += 1000)
    {
+      pattern[1] = '\0';
       cout << "\nFor operation : " << i;
-      textlen = i;
-      for(int j = 0;j < textlen - 1;j++) text[j] = 'a';
-      text[textlen - 1] = 'b';
-      pattern[0] = 'b';pattern[1] = '\0';
+      textlen = i;patternlen = (int)( textlen / ((rand() % 10) + 1) );
+      for(int j = 0;j < textlen - 1;j++) text[j] = (rand() % 26) + 'a';
+
+      strrev(text);
+      strncpy(pattern,text,patternlen);
+      strrev(text);
+
       t.start();Horspool_Machine();t.stop();
       cout << " time taken : " << t.time() << " seconds";t.reset();
    }
