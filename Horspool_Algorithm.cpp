ef76161986bc9201c1a00e382f2b78dcce695123
Nikhil Raj <rajnikhil870@gmail.com>
2016-06-18 22:23:46 +0530

    takes more characters now

--- Horspool_Algorithm.cpp
+++ Horspool_Algorithm.cpp
@@ -57,16 +57,39 @@ int Case :: Horspool_Machine()
     return -1;
 }
 
+void Case :: randomize()
+{
+    Timer t;
+    for(int i = 1000;i <= 10000;i += 1000)
+    {
+       pattern[1] = '\0';
+       cout << "\nFor operation : " << i;
+       textlen = i;patternlen = (int)( textlen / ((rand() % 10) + 1) );
+       for(int j = 0;j < textlen - 1;j++) text[j] = (rand() % 26) + 'a';
+ 
+       strrev(text);
+       strncpy(pattern,text,patternlen);
+       strrev(pattern);
+       strrev(text);
+ 
+        t.start();Horspool_Machine();t.stop();
+        cout << " time taken : " << t.time() << " seconds";t.reset();
+    }
+}
+
 int main()
 {
    clrscr();
+   Timer t;
    int index;
    Case C;
    C.input();
-   index = C.Horspool_Machine();
+   t.start();index = C.Horspool_Machine();t.stop();
 
    if(index == -1) cout << "\nMatch couldn't be found\n";
    else cout << "\nMatch found at position "<< (index + 1) <<"\n";
+   
+   cout << "\nTime taken for this is : "<< t.time() <<" seconds\n";
 
    getch();
    return 0;
